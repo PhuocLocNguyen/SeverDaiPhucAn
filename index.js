0be0db1786f5605046b9e3ae1ef509b1e2ec30a5
PhuocLoc <nguyenphuocloc095@gmail.com>
2020-07-18 08:37:13 +0700

    get id

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
-const express = require('express')
-const {json} = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const { json } = require("body-parser");
+const mongoose = require("mongoose");
 //const {Word} = require('./word');
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', './views')
-app.use(json())
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(json());
 
-app.listen(process.env.PORT || '4000' ,() => console.log("Server started"))
+app.listen(process.env.PORT || "4000", () => console.log("Server started"));
 
 mongoose.connect(
-    'mongodb+srv://reactnative18052020:Xuanthao@a@cluster0.kwuk0.mongodb.net/daiphucan?retryWrites=true&w=majority', 
-    {useNewUrlParser: true, useUnifiedTopology: true}, 
-    (err) => {
-        if(!err){
-            console.log("Mongo connected sucessfully!");
-        }
-        else{
-            console.log("Mongo err");
-        }
+  "mongodb+srv://reactnative18052020:Xuanthao@a@cluster0.kwuk0.mongodb.net/daiphucan?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("Mongo connected sucessfully!");
+    } else {
+      console.log("Mongo err");
     }
+  }
 );
 
-const Projects = require('./Models/Project');
+const Projects = require("./Models/Project");
 
-app.get('/project',(req, res) => {
-    var project = new Projects({
-        ten_du_an: 'Thuận Hòa Lucky Home',
-        chu_dau_tu: 'Công ty Bất Động Sản Thuận Hòa',
-        vi_tri: 'Đường DT741, Xã Tiến Hưng, Thành phố Đồng Xoài, Tỉnh Bình Phước',
-        hinh_anh: ['thuanhoa.jpg', 'thuanhoa1.jpg', 'thuanhoa2.jpg'],
-        loai_hinh: 'Đất nền',
-        don_vi_phan_phoi: 'Bất Động Sản Đại Phúc An',
-        quy_mo: '2.4 ha',
-        so_luong: '112 lô đất nền',
-        dien_tich_phan_lo: '106-107-111-120-128-132-140-160-180',
-        hinh_thuc_xay_dung: 'Xây dựng tự do',
-        ngan_hang: 'Viettinbank hỗ trợ cho vay từ 50% - 70%',
-        phap_ly: 'Sổ đỏ riêng từng lô',
-    });
-    // console.log(project);
-    // res.json(project);
-    project.save((err) => {
-        if(err){
-            res.send('loi');
-        }else{
-            res.json(project);
-        }
-    });
-})
-app.get('/',(req, res) => {
-    res.send('okay');
-})
- 
+app.get("/project/add", (req, res) => {
+  var project = new Projects({
+    ten_du_an: "Thuận Hòa Lucky Home",
+    chu_dau_tu: "Công ty Bất Động Sản Thuận Hòa",
+    vi_tri: "Đường DT741, Xã Tiến Hưng, Thành phố Đồng Xoài, Tỉnh Bình Phước",
+    hinh_anh: [
+      "https://daiphucan.com/wp-content/uploads/2020/02/2a13322d4b6db033e97c.jpg",
+      "https://daiphucan.com/wp-content/uploads/2020/02/Thuan-Hoa-Lucky-Home-9.jpg",
+      "https://daiphucan.com/wp-content/uploads/2020/02/Thuan-Hoa-Lucky-Home-5.jpg",
+    ],
+    loai_hinh: "Đất nền",
+    don_vi_phan_phoi: "Bất Động Sản Đại Phúc An",
+    quy_mo: "2.4 ha",
+    so_luong: "112 lô đất nền",
+    dien_tich_phan_lo: "106-107-111-120-128-132-140-160-180",
+    hinh_thuc_xay_dung: "Xây dựng tự do",
+    ngan_hang: "Viettinbank hỗ trợ cho vay từ 50% - 70%",
+    phap_ly: "Sổ đỏ riêng từng lô",
+  });
+  // console.log(project);
+  // res.json(project);
+  project.save((err) => {
+    if (err) {
+      res.send("loi");
+    } else {
+      res.json(project);
+    }
+  });
+});
+app.get("/project", (req, res) => {
+  Projects.find({})
+    .then((projects) => res.send({ success: true, projects }))
+    .catch((error) => res.send({ success: false, message: error }));
+});
+app.get("/project/:_id", (req, res) => {
+  const {_id} = req.params;
+  Projects.findById({_id})
+    .then((project) => res.send({ success: true, project}))
+    .catch((error) => res.send({ success: false, message: error }));
+});
 // app.get("/word" , (req , res) => {
 //     Word.find({})
 //     .then(words => res.send({success : true , words}))
 //     .catch(error => res.send({success : false , message : error}))
 // })
 
-// //insert 
+// //insert
 // app.post("/word" , (req , res) => {
 //     const {en , vn } = req.body
 //     if(en.trim() === '' || vn.trim() === ''){
